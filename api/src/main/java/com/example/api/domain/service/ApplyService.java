d308fc48272069511699525f51771700858fa75e
penrose15 <seongju99@naver.com>
2023-06-25 23:08:35 +0900

    feat : 쿠폰 로직 구현

---
+++ api/src/main/java/com/example/api/domain/service/ApplyService.java
@@ -0,0 +1,20 @@
+package com.example.api.domain.service;
+
+import com.example.api.domain.Coupon;
+import com.example.api.domain.repository.CouponRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ApplyService {
+    private final CouponRepository couponRepository;
+
+    public void apply(Long userId) {
+        long count = couponRepository.count();
+        if(count > 100) {
+            return;
+        }
+        couponRepository.save(new Coupon(userId));
+    }
+}
