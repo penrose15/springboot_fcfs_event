671632d57db5678a6ebfd8b05ca79c8afa7b5985
penrose15 <seongju99@naver.com>
2023-06-25 23:31:14 +0900

    test : 로직 여러번 실행시 오류 나는 케이스 추가

--- api/src/test/java/com/example/api/service/ApplyServiceTest.java
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -7,6 +7,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 public class ApplyServiceTest {
     @Autowired
@@ -20,7 +26,30 @@ public class ApplyServiceTest {
         applyService.apply(1L);
         long count = couponRepository.count();
 
-        Assertions.assertThat(count)
+        assertThat(count)
                 .isEqualTo(1L);
+    }
+
+    @Test //레이스컨디션 발생
+    public void 여러명_응모() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for(int i = 0; i<threadCount; i++) {
+            long userId = i;
+            executorService.submit(() -> {
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        long count = couponRepository.count();
+        assertThat(count)
+                .isEqualTo(100L);
     }
 }
